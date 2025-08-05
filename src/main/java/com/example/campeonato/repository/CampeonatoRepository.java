480f57ce7a4e84d5a44ad4b7fa476494a4055bc2
João Gabriel L. Silva <115912913+Gaeleite@users.noreply.github.com>
2025-08-05 14:57:26 -0300

    Delete src directory

--- src/main/java/com/example/campeonato/repository/CampeonatoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.campeonato.repository;
-
-import com.example.campeonato.model.Campeonato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampeonatoRepository extends JpaRepository<Campeonato, Integer> {
-}
--- src/main/java/com/example/campeonato/repository/ResultadoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.campeonato.repository;
-
-import com.example.campeonato.model.Resultado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultadoRepository extends JpaRepository<Resultado, Integer> {
-}
